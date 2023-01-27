76615383bdc99b03ad90b4d795b84989aab5e77b
AkseliKankaansivu <akselik11@gmail.com>
2023-01-27 14:17:15 +0200

    initial commit

---
+++ src/dataReader/CSVReader.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package dataReader;
+
+/**
+ * @author aksel
+ * @version Jan 27, 2023
+ * A CSV reader for the dataReader backend software. Fetches data from CSV type files
+ */
+public class CSVReader {
+
+    /**
+     * @param args not used
+     */
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+}
